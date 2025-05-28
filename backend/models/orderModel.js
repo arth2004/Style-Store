c5e7f2eea3944fdfc7bc3722c0b941a5379c10fa
Arth <arthagrawal72@gmail.com>
2025-05-29 02:46:21 +0530

    postalCode Number to String

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -19,7 +19,7 @@ const orderSchema = new mongoose.Schema(
     shippingAddress: {
       address: { type: String, required: true },
       city: { type: String, required: true },
-      postalCode: { type: Number, required: true },
+      postalCode: { type: String, required: true },
       country: { type: String, required: true },
     },
     paymentMethod: {
@@ -42,10 +42,10 @@ const orderSchema = new mongoose.Schema(
       required: true,
       default: 0.0,
     },
-    shippingPrice:{
-        type:Number,
-        required:true,
-        default:0.0,
+    shippingPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
     },
     totalPrice: {
       type: Number,
